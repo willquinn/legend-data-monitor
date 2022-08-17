97ab225265447b359887a6e361cef9732b4c25c8
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-08-17 15:36:02 +0200

    Test commit

--- src/legend_data_monitor/core.py
+++ src/legend_data_monitor/core.py
@@ -1,2 +1,15 @@
 def hello_world():
     return "Hello World!"
+
+
+def nuova_funzione(nome: str) -> str:
+    """Descrizione breve funzione.
+
+    Descrizione lunga.
+
+    Parameters
+    ----------
+    nome
+        nome a caso.
+    """
+    return nome
