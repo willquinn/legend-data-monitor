f35bb413be4d8dcf1a80c9c5112166db1aa87220
morellam <michele.morella@gssi.it>
2022-08-18 16:43:39 +0200

    added fixes

---
+++ src/legend_data_monitor/data_monitor.py
@@ -0,0 +1,17 @@
+def data_monitor():
+    return
+    # """
+    # Legend data monitor's command line interface.
+
+    # $ data_monitor --help
+    # """
+    # parser = argparse.ArgumentParser(
+    #     prog="data_monitor", description="legend data monitor's command line interface"
+    # )
+
+    # # global options
+    # parser.add_argument(
+    #     "--version", action="store_true", help="""Print data monitor version and exit"""
+    # )
+
+    # args = parser.parse_args()
