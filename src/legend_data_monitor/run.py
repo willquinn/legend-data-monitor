5da8cb27b379d412ab0d69e0d4f0306f4d833fdd
Sofia Calgaro <calgaro@legend-login2.lngs.infn.it>
2022-08-20 19:46:51 +0200

    fixed geds heatmmaps

--- src/legend_data_monitor/run.py
+++ src/legend_data_monitor/run.py
@@ -96,7 +96,7 @@ def dump_all_plots_together(raw_files, time_cut, path, map_path):
                                 logging.info(
                                     f"\t...{par} for geds (string #{string}) has been plotted!"
                                 )
-                        map.geds_map(geds_dict, string_geds, string_geds_name, det_status_dict, map_path, pdf_map)
+                    map.geds_map(geds_dict, string_geds, string_geds_name, det_status_dict, map_path, pdf_map)
 
             # Spms plots
             if det_type["spms"] is True:
