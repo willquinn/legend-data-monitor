94164e0fa421a053df12523f892fd1700b093f9f
Sofia Calgaro <calgaro@legend-login1.lngs.infn.it>
2022-08-20 15:07:17 +0200

    fixed pre-commit errors

--- src/legend_data_monitor/run.py
+++ src/legend_data_monitor/run.py
@@ -221,10 +221,8 @@ def main():
         dirs = ["pdf-files", "pkl-files"]
         if out_dir in dirs:
            for out_subdir in ["par-vs-time", "heatmaps"]:
-               if os.path.isdir(f'{cwd_path}{out_dir}/{out_subdir}')==False:
+               if os.path.isdir(f'{cwd_path}{out_dir}/{out_subdir}') is False:
                   os.mkdir(f'{cwd_path}{out_dir}/{out_subdir}')
-               else:
-                  continue
 
     plot_path = pdf_path + "/par-vs-time"
     map_path = pdf_path + "/heatmaps"
