25405e507d2ece1180086df6c7a3db5a30bbdb3c
Sofia Calgaro <77326044+sofia-calgaro@users.noreply.github.com>
2022-08-28 18:32:17 +0200

    Update plot.py

--- src/legend_data_monitor/plot.py
+++ src/legend_data_monitor/plot.py
@@ -425,8 +425,7 @@ def plot_par_vs_time_ch000(
 
         # skip the file if dsp-parameter is not present in the dsp file
         if j_par[0][parameter]["tier"] == 2:
-            parameter = "ch000/dsp/" + parameter
-            if parameter not in lh5.ls(dsp_file, "ch000/dsp/"):
+            if f"ch000/dsp/{parameter}" not in lh5.ls(dsp_file, "ch000/dsp/"):
                 continue
 
         # skip the detector if not in raw file
