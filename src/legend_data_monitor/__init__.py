77e9f6b07d5b88154713326b1660740b2b490eb0
Sofia Calgaro <calgaro@legend-login2.lngs.infn.it>
2023-07-04 09:53:28 +0200

    changed name for SC module

--- src/legend_data_monitor/__init__.py
+++ src/legend_data_monitor/__init__.py
@@ -1,7 +1,7 @@
 from legend_data_monitor._version import version as __version__
 from legend_data_monitor.analysis_data import AnalysisData
 from legend_data_monitor.core import control_plots
-from legend_data_monitor.plot_sc import SlowControl
+from legend_data_monitor.slow_control import SlowControl
 from legend_data_monitor.subsystem import Subsystem
 
 __all__ = [
